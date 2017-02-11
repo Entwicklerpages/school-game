d3469e5595edf436148b02ffd9f1dbba076896bc
Entwicklerpages <nicolas.hollmann@web.de>
2017-02-11 11:58:55 +0100

    libGDX installation
    gdx-setup done its work!

---
+++ desktop/src/de/entwicklerpages/java/schoolgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.entwicklerpages.java.schoolgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.entwicklerpages.java.schoolgame.SchoolGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SchoolGame(), config);
+	}
+}
---
+++ html/src/de/entwicklerpages/java/schoolgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.entwicklerpages.java.schoolgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.entwicklerpages.java.schoolgame.SchoolGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SchoolGame();
+        }
+}
