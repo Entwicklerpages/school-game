04bd69cfd984e8d0b44c04836a75796910ddcd85
Entwicklerpages <Entwicklerpages@users.noreply.github.com>
2017-02-23 18:19:32 +0100

    Fixed identation
    These files were added by the libgdx setup and used tabs instead of 4
    spaces. Now they fit to the other files, but git thinks they are
    completely different files.

--- core/src/de/entwicklerpages/java/schoolgame/SchoolGame.java
+++ core/src/de/entwicklerpages/java/schoolgame/SchoolGame.java
@@ -14,203 +14,203 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import de.entwicklerpages.java.schoolgame.menu.Splashscreen;
 
 public class SchoolGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private Viewport viewport;
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
-	private InputMultiplexer inputMultiplexer;
-	private Preferences preferences;
-	private BitmapFont defaultFont;
-	private BitmapFont longTextFont;
-	private BitmapFont titleFont;
+    private InputMultiplexer inputMultiplexer;
+    private Preferences preferences;
+    private BitmapFont defaultFont;
+    private BitmapFont longTextFont;
+    private BitmapFont titleFont;
 
-	private GameState gameState;
+    private GameState gameState;
 
-	/***
-	 * Wird zum Start einmal aufgerufen.
-	 * Hier werden alle benötigten Variablen initialisiert.
-	 */
-	@Override
-	public void create() {
-		Gdx.app.getApplicationLogger().log("INFO", "Init game...");
-
-		camera = new OrthographicCamera();
-
-		viewport = new FitViewport(1280, 720, camera);
-		viewport.apply();
-
-		gameState = new Splashscreen();
-		gameState.create(this);
-
-		inputMultiplexer = new InputMultiplexer();
-		Gdx.input.setInputProcessor(inputMultiplexer);
-
-		preferences = Gdx.app.getPreferences("de.entwicklerpages.java.schoolgame");
-
-		String fontName = "Niconne";
-		if (!preferences.contains("use_pixel_font")) {
-			preferences.putBoolean("use_pixel_font", false);
-			preferences.flush();
-		}
-
-		if (preferences.getBoolean("use_pixel_font", false))
-		{
-			fontName = "VT323";
-		}
-
-		defaultFont = new BitmapFont(Gdx.files.internal("font/" + fontName + "_60.fnt"), false);
-		//defaultFont.getData().setScale(4); // Skalierung (sollte vermieden werden)
-
-		longTextFont = new BitmapFont(Gdx.files.internal("font/" + fontName + "_30.fnt"), false);
-		titleFont = new BitmapFont(Gdx.files.internal("font/" + fontName + "_76.fnt"), false);
-
-		if (shouldBeFullscreen())
-		{
-			Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
-			Gdx.input.setCursorCatched(true);
-		} else if (preferences.contains("window_width") && preferences.contains("window_height")) {
-			Gdx.graphics.setWindowedMode(preferences.getInteger("window_width"), preferences.getInteger("window_height"));
-		}
-		Gdx.graphics.setVSync(shouldVSync());
-
-		Gdx.app.getApplicationLogger().log("INFO", "Finished.");
-	}
-
-	/***
-	 * Wird aufgerufen, wenn sich die Fenstergröße ändert.
-	 *
-	 * @param width	Die neue Breite des Fensters
-	 * @param height Die neue Höhe des Fensters
+    /***
+     * Wird zum Start einmal aufgerufen.
+     * Hier werden alle benötigten Variablen initialisiert.
      */
-	@Override
-	public void resize(int width, int height) {
-		viewport.update(width, height);
-	}
-
-	/***
-	 * Hauptschleife des Spiels.
-	 */
-	@Override
-	public void render() {
-
-		camera.update();
-
-		gameState.update(Gdx.graphics.getDeltaTime());
-
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-
-		gameState.render(camera, Gdx.graphics.getDeltaTime());
-	}
-
-	/***
-	 * Wird kurz vor dem Beenden aufgerufen.
-	 * Brauchen wir nicht.
- 	 */
     @Override
-	public void pause() {
-		if (!Gdx.graphics.isFullscreen()) {
-			preferences.putInteger("window_width", Gdx.graphics.getWidth());
-			preferences.putInteger("window_height", Gdx.graphics.getHeight());
-		}
-		preferences.flush();
+    public void create() {
+        Gdx.app.getApplicationLogger().log("INFO", "Init game...");
 
-		Gdx.input.setCursorCatched(false);
-	}
+        camera = new OrthographicCamera();
 
-	/**
-	 * Wird auf der Desktop-Platform nie aufgerufen.
-	 */
-	@Override
-	public void resume() {
-	}
+        viewport = new FitViewport(1280, 720, camera);
+        viewport.apply();
 
-	/***
-	 * Hier werden alle Resourcen freigegeben.
-	 */
-	@Override
-	public void dispose() {
-		Gdx.app.getApplicationLogger().log("INFO", "Dispose resources...");
+        gameState = new Splashscreen();
+        gameState.create(this);
 
-		gameState.dispose();
+        inputMultiplexer = new InputMultiplexer();
+        Gdx.input.setInputProcessor(inputMultiplexer);
 
-		defaultFont.dispose();
-		longTextFont.dispose();
-		titleFont.dispose();
+        preferences = Gdx.app.getPreferences("de.entwicklerpages.java.schoolgame");
 
-		Gdx.app.getApplicationLogger().log("INFO", "Finished.");
-		Gdx.app.getApplicationLogger().log("INFO", "Quit.");
-	}
+        String fontName = "Niconne";
+        if (!preferences.contains("use_pixel_font")) {
+            preferences.putBoolean("use_pixel_font", false);
+            preferences.flush();
+        }
 
-	public void addInputProcessor(InputProcessor inputProcessor)
-	{
-		inputMultiplexer.addProcessor(inputProcessor);
-	}
+        if (preferences.getBoolean("use_pixel_font", false))
+        {
+            fontName = "VT323";
+        }
 
-	public void removeInputProccessor(InputProcessor inputProcessor)
-	{
-		inputMultiplexer.removeProcessor(inputProcessor);
-	}
+        defaultFont = new BitmapFont(Gdx.files.internal("font/" + fontName + "_60.fnt"), false);
+        //defaultFont.getData().setScale(4); // Skalierung (sollte vermieden werden)
 
-	public void setGameState(GameState newState)
-	{
-		if (newState == null)
-			return;
+        longTextFont = new BitmapFont(Gdx.files.internal("font/" + fontName + "_30.fnt"), false);
+        titleFont = new BitmapFont(Gdx.files.internal("font/" + fontName + "_76.fnt"), false);
 
-		Gdx.app.getApplicationLogger().log("INFO", "Change state from " + gameState.getStateName() + " to " + newState.getStateName());
+        if (shouldBeFullscreen())
+        {
+            Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+            Gdx.input.setCursorCatched(true);
+        } else if (preferences.contains("window_width") && preferences.contains("window_height")) {
+            Gdx.graphics.setWindowedMode(preferences.getInteger("window_width"), preferences.getInteger("window_height"));
+        }
+        Gdx.graphics.setVSync(shouldVSync());
 
-		gameState.dispose();
+        Gdx.app.getApplicationLogger().log("INFO", "Finished.");
+    }
 
-		inputMultiplexer.clear();
+    /***
+     * Wird aufgerufen, wenn sich die Fenstergröße ändert.
+     *
+     * @param width    Die neue Breite des Fensters
+     * @param height Die neue Höhe des Fensters
+     */
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
 
-		camera.position.set(0, 0, 0);
+    /***
+     * Hauptschleife des Spiels.
+     */
+    @Override
+    public void render() {
 
-		gameState = newState;
+        camera.update();
 
-		gameState.create(this);
+        gameState.update(Gdx.graphics.getDeltaTime());
 
-		if (gameState instanceof InputProcessor) {
-			inputMultiplexer.addProcessor((InputProcessor) gameState);
-		}
-	}
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	public Preferences getPreferences()
-	{
-		return preferences;
-	}
 
-	public boolean shouldBeFullscreen()
-	{
-		return preferences.getBoolean("fullscreen", false);
-	}
+        gameState.render(camera, Gdx.graphics.getDeltaTime());
+    }
 
-	public void setFullscreen(boolean fullscreen)
-	{
-		preferences.putBoolean("fullscreen", fullscreen);
-	}
+    /***
+     * Wird kurz vor dem Beenden aufgerufen.
+     * Brauchen wir nicht. 
+     */
+    @Override
+    public void pause() {
+        if (!Gdx.graphics.isFullscreen()) {
+            preferences.putInteger("window_width", Gdx.graphics.getWidth());
+            preferences.putInteger("window_height", Gdx.graphics.getHeight());
+        }
+        preferences.flush();
 
-	public boolean shouldVSync()
-	{
-		return preferences.getBoolean("vsync", false);
-	}
+        Gdx.input.setCursorCatched(false);
+    }
 
-	public void setVSync(boolean vsync)
-	{
-		preferences.putBoolean("vsync", vsync);
-	}
+    /**
+     * Wird auf der Desktop-Platform nie aufgerufen.
+     */
+    @Override
+    public void resume() {
+    }
 
-	public BitmapFont getDefaultFont()
-	{
-		return defaultFont;
-	}
+    /***
+     * Hier werden alle Resourcen freigegeben.
+     */
+    @Override
+    public void dispose() {
+        Gdx.app.getApplicationLogger().log("INFO", "Dispose resources...");
 
-	public BitmapFont getLongTextFont()
-	{
-		return longTextFont;
-	}
+        gameState.dispose();
 
-	public BitmapFont getTitleFont()
-	{
-		return titleFont;
-	}
+        defaultFont.dispose();
+        longTextFont.dispose();
+        titleFont.dispose();
+
+        Gdx.app.getApplicationLogger().log("INFO", "Finished.");
+        Gdx.app.getApplicationLogger().log("INFO", "Quit.");
+    }
+
+    public void addInputProcessor(InputProcessor inputProcessor)
+    {
+        inputMultiplexer.addProcessor(inputProcessor);
+    }
+
+    public void removeInputProccessor(InputProcessor inputProcessor)
+    {
+        inputMultiplexer.removeProcessor(inputProcessor);
+    }
+
+    public void setGameState(GameState newState)
+    {
+        if (newState == null)
+            return;
+
+        Gdx.app.getApplicationLogger().log("INFO", "Change state from " + gameState.getStateName() + " to " + newState.getStateName());
+
+        gameState.dispose();
+
+        inputMultiplexer.clear();
+
+        camera.position.set(0, 0, 0);
+
+        gameState = newState;
+
+        gameState.create(this);
+
+        if (gameState instanceof InputProcessor) {
+            inputMultiplexer.addProcessor((InputProcessor) gameState);
+        }
+    }
+
+    public Preferences getPreferences()
+    {
+        return preferences;
+    }
+
+    public boolean shouldBeFullscreen()
+    {
+        return preferences.getBoolean("fullscreen", false);
+    }
+
+    public void setFullscreen(boolean fullscreen)
+    {
+        preferences.putBoolean("fullscreen", fullscreen);
+    }
+
+    public boolean shouldVSync()
+    {
+        return preferences.getBoolean("vsync", false);
+    }
+
+    public void setVSync(boolean vsync)
+    {
+        preferences.putBoolean("vsync", vsync);
+    }
+
+    public BitmapFont getDefaultFont()
+    {
+        return defaultFont;
+    }
+
+    public BitmapFont getLongTextFont()
+    {
+        return longTextFont;
+    }
+
+    public BitmapFont getTitleFont()
+    {
+        return titleFont;
+    }
 }
--- desktop/src/de/entwicklerpages/java/schoolgame/desktop/DesktopLauncher.java
+++ desktop/src/de/entwicklerpages/java/schoolgame/desktop/DesktopLauncher.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.entwicklerpages.java.schoolgame.SchoolGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "School Game";
-		config.useGL30 = false;
-		config.forceExit = true;
-		new LwjglApplication(new SchoolGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "School Game";
+        config.useGL30 = false;
+        config.forceExit = true;
+        new LwjglApplication(new SchoolGame(), config);
+    }
 }
