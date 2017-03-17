8f6dd62f2390974db666fac063bddcea328d05b4
Spirupi <Spirupi@users.noreply.github.com>
2017-03-17 11:01:04 +0100

    Added tree view to dialog editor

--- tools/src/de/entwicklerpages/java/schoolgame/tools/dialog/DialogEditor.java
+++ tools/src/de/entwicklerpages/java/schoolgame/tools/dialog/DialogEditor.java
@@ -1,12 +1,60 @@
 package de.entwicklerpages.java.schoolgame.tools.dialog;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 public class DialogEditor extends JPanel {
 
+    private JTree treeView = null;
+    private JButton saveButton = null;
+    private JButton loadButton = null;
+
     public DialogEditor()
     {
-        super();
+        super(new BorderLayout());
+
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Level");
+
+        DefaultMutableTreeNode charactersNode = new DefaultMutableTreeNode( "Charaktere" );
+        root.add( charactersNode );
+
+        DefaultMutableTreeNode dialogNode = new DefaultMutableTreeNode(  "Dialoge" );
+        root.add( dialogNode );
+
+        for ( int nodeCnt = 0; nodeCnt < 4; nodeCnt++ )
+        {
+            DefaultMutableTreeNode dmtn = new DefaultMutableTreeNode( "Knoten " + nodeCnt );
+            dialogNode.add( dmtn );
+
+            for ( int leafCnt = 1; leafCnt < 4; leafCnt++ )
+                dmtn.add( new DefaultMutableTreeNode( "Blatt " + (nodeCnt * 3 + leafCnt) ) );
+        }
+
+
+        treeView = new JTree(root);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.add(new JScrollPane(treeView));
+        splitPane.add(new JPanel());
+
+        add(splitPane, BorderLayout.CENTER);
+
+        JPanel saveBar = new JPanel(new GridLayout(1, 2));
+
+        loadButton = new JButton("Laden");
+        saveButton = new JButton("Speichern");
+
+        saveBar.add(loadButton);
+        saveBar.add(saveButton);
+
+        add(saveBar, BorderLayout.SOUTH);
     }
 
 }
