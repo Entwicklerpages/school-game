75f1c40b420098ca4949fd837d1606482a4e6051
Entwicklerpages <Entwicklerpages@users.noreply.github.com>
2017-03-17 18:09:17 +0100

    Added level loading to tool
    Added the possibility to load a level to the tool. Also added schema
    validation.

--- tools/src/de/entwicklerpages/java/schoolgame/tools/dialog/DialogDataHelper.java
+++ tools/src/de/entwicklerpages/java/schoolgame/tools/dialog/DialogDataHelper.java
@@ -1,11 +1,16 @@
 package de.entwicklerpages.java.schoolgame.tools.dialog;
 
+import org.xml.sax.SAXException;
+
 import java.io.File;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import de.entwicklerpages.java.schoolgame.game.dialog.Level;
 
@@ -23,19 +28,47 @@ public final class DialogDataHelper {
         return jaxbContext;
     }
 
-    public static Level getDialogRoot(File dialogFile) throws JAXBException
+    public static Level getDialogRoot(File dialogFile) throws JAXBException, SAXException
     {
         Unmarshaller unmarshaller = getJaxbContext().createUnmarshaller();
-        //unmarshaller.setSchema(dialogSchema);
+        unmarshaller.setSchema(getSchema());
 
         return (Level) unmarshaller.unmarshal(dialogFile);
     }
 
-    public static void saveDialogRoot(File dialogFile, Level root) throws JAXBException {
+    public static void saveDialogRoot(File dialogFile, Level root) throws JAXBException, SAXException
+    {
 
         Marshaller marshaller = getJaxbContext().createMarshaller();
-        //marshaller.setSchema(dialogSchema);
+        marshaller.setSchema(getSchema());
 
         marshaller.marshal(root, dialogFile);
+    }
+
+    public static File getAssetDirIfFound()
+    {
+        File workingDir = new File(System.getProperty("user.dir"));
+
+        File dialogAssetDir = new File(workingDir.getAbsolutePath() + File.separator + "core" + File.separator + "assets" + File.separator + "dialog");
+
+        if (dialogAssetDir.exists() && dialogAssetDir.isDirectory())
+        {
+            return dialogAssetDir;
+        }
+
+        return workingDir;
+    }
+
+    private static Schema getSchema() throws SAXException
+    {
+        File schemaFile = new File(getAssetDirIfFound().getAbsolutePath() + File.separator + "dialog.xsd");
+
+        if (!schemaFile.exists() || !schemaFile.canRead())
+            return null;
+
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = factory.newSchema(schemaFile);
+
+        return schema;
     }
 }
--- tools/src/de/entwicklerpages/java/schoolgame/tools/dialog/DialogEditor.java
+++ tools/src/de/entwicklerpages/java/schoolgame/tools/dialog/DialogEditor.java
@@ -2,54 +2,61 @@ package de.entwicklerpages.java.schoolgame.tools.dialog;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTree;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
-public class DialogEditor extends JPanel {
+import de.entwicklerpages.java.schoolgame.game.dialog.CharacterType;
+import de.entwicklerpages.java.schoolgame.game.dialog.DialogType;
+import de.entwicklerpages.java.schoolgame.game.dialog.Level;
+
+public class DialogEditor extends JPanel implements ActionListener {
+
+    private Level level = null;
 
     private JTree treeView = null;
     private JButton saveButton = null;
     private JButton loadButton = null;
 
+    private JPanel containerPanel = new JPanel();
+
+    private DefaultMutableTreeNode root = null;
+
     public DialogEditor()
     {
         super(new BorderLayout());
 
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Level");
-
-        DefaultMutableTreeNode charactersNode = new DefaultMutableTreeNode( "Charaktere" );
-        root.add( charactersNode );
-
-        DefaultMutableTreeNode dialogNode = new DefaultMutableTreeNode(  "Dialoge" );
-        root.add( dialogNode );
-
-        for ( int nodeCnt = 0; nodeCnt < 4; nodeCnt++ )
-        {
-            DefaultMutableTreeNode dmtn = new DefaultMutableTreeNode( "Knoten " + nodeCnt );
-            dialogNode.add( dmtn );
-
-            for ( int leafCnt = 1; leafCnt < 4; leafCnt++ )
-                dmtn.add( new DefaultMutableTreeNode( "Blatt " + (nodeCnt * 3 + leafCnt) ) );
-        }
-
-
+        root = new DefaultMutableTreeNode("Level");
+        buildTree();
         treeView = new JTree(root);
 
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         splitPane.add(new JScrollPane(treeView));
-        splitPane.add(new JPanel());
+        splitPane.add(containerPanel);
+        splitPane.setResizeWeight(0.25);
 
         add(splitPane, BorderLayout.CENTER);
 
         JPanel saveBar = new JPanel(new GridLayout(1, 2));
 
         loadButton = new JButton("Laden");
+        loadButton.addActionListener(this);
+
         saveButton = new JButton("Speichern");
+        saveButton.addActionListener(this);
 
         saveBar.add(loadButton);
         saveBar.add(saveButton);
@@ -57,4 +64,77 @@ public class DialogEditor extends JPanel {
         add(saveBar, BorderLayout.SOUTH);
     }
 
+    private void buildTree()
+    {
+        root.removeAllChildren();
+
+        if (level == null)
+            return;
+
+        DefaultMutableTreeNode charactersNode = new DefaultMutableTreeNode("Charaktere");
+        root.add(charactersNode);
+
+        List<CharacterType> characters = level.getCharacters().getCharacter();
+        for (CharacterType character : characters) {
+            charactersNode.add(new DefaultMutableTreeNode(character.getId().length() == 0 ? "Charakter" : character.getId()));
+        }
+
+        DefaultMutableTreeNode dialogsNode = new DefaultMutableTreeNode("Dialoge");
+        root.add(dialogsNode);
+
+        List<DialogType> dialogs = level.getDialogs().getDialog();
+        for (DialogType dialog : dialogs) {
+            dialogsNode.add(new DefaultMutableTreeNode(dialog.getName().length() == 0 ? "Dialog" : dialog.getName()));
+        }
+    }
+
+    private void rebuildTree()
+    {
+        buildTree();
+
+        if (treeView != null)
+            ((DefaultTreeModel) treeView.getModel()).reload(root);
+    }
+
+    private void loadLevel()
+    {
+        FileFilter filter = new FileNameExtensionFilter("Dialog XML Datei", "xml");
+        JFileChooser chooser = new JFileChooser(DialogDataHelper.getAssetDirIfFound());
+        chooser.addChoosableFileFilter(filter);
+        chooser.setAcceptAllFileFilterUsed(true);
+
+        int result = chooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION)
+        {
+            level = null;
+            File selectedFile = chooser.getSelectedFile();
+            try {
+                level = DialogDataHelper.getDialogRoot(selectedFile);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(this, "Beim Laden ist ein Fehler aufgetreten!\n" + e.getLocalizedMessage(), "Fehler!", JOptionPane.OK_OPTION);
+            }
+            rebuildTree();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource() == loadButton)
+        {
+            if (level != null)
+            {
+                int result = JOptionPane.showConfirmDialog(this, "Achtung!\n" +
+                        "Es wird ein neues Level geladen.\n" +
+                        "Ungespeicherte Änderungen gehen verloren!\n" +
+                        "Wollen Sie wirklich ein anderes Level laden?", "Level laden", JOptionPane.YES_NO_OPTION);
+
+                if (result == JOptionPane.YES_OPTION) {
+                    loadLevel();
+                }
+            } else {
+                loadLevel();
+            }
+        }
+    }
 }
