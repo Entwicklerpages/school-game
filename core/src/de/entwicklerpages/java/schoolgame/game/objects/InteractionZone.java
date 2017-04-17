aea43afe58b4814948ca24f59a7ba87f1b15cde9
Entwicklerpages <Entwicklerpages@users.noreply.github.com>
2017-04-17 18:35:15 +0200

    Interaction system
    Added a system which reacts to player interactions. Custom interaction
    handler can be defined in the Level subclasses.

--- core/src/de/entwicklerpages/java/schoolgame/game/objects/InteractionZone.java
+++ core/src/de/entwicklerpages/java/schoolgame/game/objects/InteractionZone.java
@@ -1,12 +1,59 @@
 package de.entwicklerpages.java.schoolgame.game.objects;
 
+import de.entwicklerpages.java.schoolgame.common.ActionCallback;
+
 /**
  * @author nico
  */
 public class InteractionZone extends WorldObject
 {
+    private int priority;
+    private InteractionHandler handler;
+    private ActionCallback actionCallback = null;
+    private InteractionHandler.InteractionCallback interactionCallback = null;
+
     public InteractionZone(String objectId)
     {
+        this(objectId, 0);
+    }
+
+    public InteractionZone(String objectId, int priority)
+    {
         super(objectId);
+
+        this.priority = priority;
+    }
+
+    @Override
+    public void onInit()
+    {
+        handler = new InteractionHandler(worldObjectManager, priority);
+        handler.createStaticInteractionBody(rawObject);
+        handler.setInteractionCallback(interactionCallback);
+        handler.setActionCallback(actionCallback);
+    }
+
+    public void setPriority(int priority)
+    {
+        if (handler != null)
+            handler.setPriority(priority);
+        else
+            this.priority = priority;
+    }
+
+    public void setInteractionCallback(InteractionHandler.InteractionCallback interactionCallback)
+    {
+        if (handler != null)
+            handler.setInteractionCallback(interactionCallback);
+        else
+            this.interactionCallback = interactionCallback;
+    }
+
+    public void setActionCallback(ActionCallback actionCallback)
+    {
+        if (handler != null)
+            handler.setActionCallback(actionCallback);
+        else
+            this.actionCallback = actionCallback;
     }
 }
