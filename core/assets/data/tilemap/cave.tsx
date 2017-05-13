757d0c5ed82e597f6ca14073a232d304a74c2dba
Entwicklerpages <Entwicklerpages@users.noreply.github.com>
2017-05-13 12:34:42 +0200

    Cave tileset and player cleanup
    Added a cave tileset.
    
    The male player animation now is clean.

--- core/assets/data/tilemap/cave.tsx
+++ core/assets/data/tilemap/cave.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="forest" tilewidth="32" tileheight="32" tilecount="56" columns="8">
- <image source="forest_map.png" width="256" height="224"/>
+<tileset name="cave" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="cave_map.png" width="256" height="192"/>
  <terraintypes>
   <terrain name="Wasser" tile="39"/>
-  <terrain name="Rasen" tile="16"/>
-  <terrain name="Sand" tile="42"/>
+  <terrain name="Boden" tile="25"/>
+  <terrain name="Wand" tile="20"/>
  </terraintypes>
  <tile id="2" terrain="1,1,1,0">
   <animation>
@@ -42,7 +42,12 @@
    <frame tileid="11" duration="300"/>
   </animation>
  </tile>
- <tile id="16" terrain="1,1,1,1"/>
+ <tile id="16" terrain="2,2,2,1"/>
+ <tile id="17" terrain="2,2,1,1"/>
+ <tile id="18" terrain="2,2,1,2"/>
+ <tile id="19" terrain="2,2,2,2"/>
+ <tile id="20" terrain="2,2,2,2"/>
+ <tile id="21" terrain="2,2,2,2"/>
  <tile id="22" terrain="0,0,0,1">
   <animation>
    <frame tileid="6" duration="300"/>
@@ -55,12 +60,10 @@
    <frame tileid="23" duration="300"/>
   </animation>
  </tile>
- <tile id="24" terrain="2,2,2,1"/>
- <tile id="25" terrain="2,2,1,2"/>
+ <tile id="24" terrain="2,1,2,1"/>
+ <tile id="25" terrain="1,1,1,1"/>
  <tile id="26" terrain="1,2,1,2"/>
- <tile id="27" terrain="2,1,2,1"/>
- <tile id="28" terrain="1,1,1,2"/>
- <tile id="29" terrain="1,1,2,1"/>
+ <tile id="29" terrain="2,2,,"/>
  <tile id="30" terrain="0,1,0,0">
   <animation>
    <frame tileid="14" duration="300"/>
@@ -74,28 +77,28 @@
   </animation>
  </tile>
  <tile id="32" terrain="2,1,2,2"/>
- <tile id="33" terrain="1,2,2,2"/>
- <tile id="34" terrain="1,1,2,2"/>
- <tile id="35" terrain="2,2,1,1"/>
- <tile id="36" terrain="1,2,1,1"/>
- <tile id="37" terrain="2,1,1,1"/>
+ <tile id="33" terrain="1,1,2,2"/>
+ <tile id="34" terrain="1,2,2,2"/>
+ <tile id="35" terrain="2,,2,"/>
+ <tile id="36" terrain=",2,,2"/>
+ <tile id="37" terrain=",,2,2"/>
  <tile id="39" terrain="0,0,0,0">
   <animation>
    <frame tileid="38" duration="300"/>
    <frame tileid="39" duration="300"/>
   </animation>
  </tile>
- <tile id="42" terrain="2,2,2,2"/>
+ <tile id="42" terrain="1,1,1,1"/>
+ <tile id="45" terrain="1,1,0,0">
+  <animation>
+   <frame tileid="44" duration="300"/>
+   <frame tileid="45" duration="300"/>
+  </animation>
+ </tile>
  <tile id="47" terrain="0,0,1,1">
   <animation>
    <frame tileid="47" duration="300"/>
    <frame tileid="46" duration="300"/>
-  </animation>
- </tile>
- <tile id="55" terrain="1,1,0,0">
-  <animation>
-   <frame tileid="54" duration="300"/>
-   <frame tileid="55" duration="300"/>
   </animation>
  </tile>
 </tileset>
