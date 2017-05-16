0e1bd7975ff0c7ac11ce743d9edbef9130ca1342
Entwicklerpages <Entwicklerpages@users.noreply.github.com>
2017-05-16 15:10:31 +0200

    Added new tilesets

---
+++ core/assets/data/tilemap/village_mayor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="village_mayor" tilewidth="96" tileheight="96" tilecount="1" columns="1">
+ <image source="village_mayor.png" width="96" height="96"/>
+</tileset>
